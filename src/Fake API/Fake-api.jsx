a5f8696796a831d1a3e953c2dfc0b4d553cd874b
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-03-15 09:08:32 +0700

    masuk

---
+++ src/Fake API/Fake-api.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class FakeApi extends Component {
+    render() {
+        return (
+            <div>
+                <h1>masuk fake api</h1>
+            </div>
+        );
+    }
+}
+
+export default FakeApi;
